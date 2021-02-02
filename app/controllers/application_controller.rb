1c19ed7d715a1c5bf98cc88fa1cdcf61f24d1e51
Noah Adams <nhadams10@uchicago.edu>
2021-02-02 04:52:01 +0000

    finished assignment got 100% on all the tests!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -34,4 +34,19 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "game_templates/user_paper.html.erb" })
   end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
+
 end
