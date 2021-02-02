edeaa3d22f1f1de7f8d7de098fe1212445e42e41
Noah Adams <nhadams10@uchicago.edu>
2021-02-02 04:31:26 +0000

    Loads of progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,34 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-    def play_rock
-      # write a ton of code
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
-      # redirect_to("https://www.wikipedia.org")
 
-      # render({ :plain => "Howdy, world!" })
+  def play_rock
+    # write a ton of code
 
-      #render({ :html => "<h1>Howdy, world!</h1>".html_safe })
+    # redirect_to("https://www.wikipedia.org")
 
-      render({ :template => "game_templates/user_rock.html.erb"})
+    # render({ :plain => "Howdy, world!" })
+
+    #render({ :html => "<h1>Howdy, world!</h1>".html_safe })
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
     end
+
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
 end
