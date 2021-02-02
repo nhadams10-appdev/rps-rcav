ee4666c48a971d8bb1f516486972eda2965d01a0
Noah Adams <nhadams10@uchicago.edu>
2021-02-02 04:01:21 +0000

    Rock page - got playing working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+    def play_rock
+      # write a ton of code
+
+      # redirect_to("https://www.wikipedia.org")
+
+      # render({ :plain => "Howdy, world!" })
+
+      #render({ :html => "<h1>Howdy, world!</h1>".html_safe })
+
+      render({ :template => "game_templates/user_rock.html.erb"})
+    end
 end
